b0a7a907f824e77a8558bfa81767b824e7c2d45d
soomskim <61723783+soomskim@users.noreply.github.com>
2022-06-07 05:33:07 -0400

    Add files via upload

---
+++ js/slideshow.js
@@ -0,0 +1,24 @@
+//img gallery - slideshow
+var slideIndex = [1];
+var slideId = ["slide"]
+showSlides(1, 0);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function currentSlide(n, no) {
+  showSlides(slideIndex[no] = n, no);
+}
+
+function showSlides(n, no) {
+  var i;
+  var slides = document.getElementsByClassName(slideId[no]);
+  if (n > slides.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+
+  }
+  slides[slideIndex[no]-1].style.display = "block";  
+}
